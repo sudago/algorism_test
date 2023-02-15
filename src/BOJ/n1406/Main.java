c2b658ca45eb5a6e1f79cf52b01d214132307fdf
sudago <sudago22@gmail.com>
2023-02-15 16:27:57 +0900

    Refactor : Solve 1406, 에디터
    refactor BOJ time out

--- src/BOJ/n1406/Main.java
+++ src/BOJ/n1406/Main.java
@@ -3,52 +3,55 @@ package BOJ.n1406;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
-// 에디터 - 시간초과 => O(1) 이어야함.
+/*
+ 에디터
+ 시간초과 => O(1) 이어야함.
+ = ArrayList -> Stack 변경
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        // 이게 문젠가
-        List<String> stringList = new ArrayList<>(List.of(br.readLine().split("")));
+
+        String[] strings = br.readLine().split("");
+        Stack<String> leftStack = new Stack<>();
+        Stack<String> rightStack = new Stack<>();
+        for (String str : strings) leftStack.push(str);
+
         int instructionNumber = Integer.parseInt(br.readLine());
-        int cursor = stringList.size(); // cursor 의 index 위치
 
         while (instructionNumber-- > 0) {
             String[] instruction = br.readLine().split(" ");
 
-            // 이게 문젠가
             switch (instruction[0]) {
                 // 커서의 위치를 왼쪽으로 바꾼다.
                 case "L" :
-                    if (cursor > 0) cursor--;
+                    if (!leftStack.isEmpty()) rightStack.push(leftStack.pop());
                     break;
                 // 커서의 위치를 오른쪽으로 바꾼다.
                 case "D" :
-                    if (cursor < stringList.size()) cursor++;
+                    if (!rightStack.isEmpty()) leftStack.push(rightStack.pop());
                     break;
                 // 커서의 왼쪽 요소를 지운다.
                 case "B" :
-                    if (cursor > 0) {
-                        stringList.remove(cursor-1);
-                        cursor--;
-                    }
+                    if (!leftStack.isEmpty()) leftStack.pop();
                     break;
                 // instruction[1] 의 값을 커서의 왼쪽에 삽입한다.
                 case "P" :
-                    stringList.add(cursor, instruction[1]);
-                    cursor++;
+                    leftStack.push(instruction[1]);
                     break;
             } // switch - end
         } // while - end
 
-        for (String ch : stringList) {
-            sb.append(ch);
+        while (!leftStack.isEmpty()) {
+            rightStack.push(leftStack.pop());
         }
 
+        while (!rightStack.isEmpty()){
+            sb.append(rightStack.pop());
+        }
         System.out.println(sb);
-
     }
 }
